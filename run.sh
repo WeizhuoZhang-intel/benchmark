7747e9ef218c2be2c30bb5370b260d4bcb10b0d3
Weizhuo Zhang <weizhuo.zhang@intel.com>
2023-03-30 21:21:12 +0800

    fix syntax issue

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 set -x
 
-bash launch_benchmark.sh all throughput "--num-iter 1"
-bash launch_benchmark.sh time_long throughput "--num-iter 1"
+#bash launch_benchmark.sh all throughput "--num-iter 1"
+#bash launch_benchmark.sh time_long throughput "--num-iter 1"
 
 mode_all=$1
 if [ ${mode_all} == "all" ]; then
@@ -18,9 +18,9 @@ do
     bash launch_benchmark.sh ${model} ${mode} "-m eager --channels-last"
     bash launch_benchmark.sh ${model} ${mode} "-m jit --channels-last"
 
-    model="time_long"
-    bash launch_benchmark.sh ${model} ${mode} "--num-iter 10 -m eager"
-    bash launch_benchmark.sh ${model} ${mode} "--num-iter 10 -m jit "
-    bash launch_benchmark.sh ${model} ${mode} "--num-iter 10 -m eager --channels-last"
-    bash launch_benchmark.sh ${model} ${mode} "--num-iter 10 -m jit --channels-last"
+    #model="time_long"
+    #bash launch_benchmark.sh ${model} ${mode} "--num-iter 10 -m eager"
+    #bash launch_benchmark.sh ${model} ${mode} "--num-iter 10 -m jit "
+    #bash launch_benchmark.sh ${model} ${mode} "--num-iter 10 -m eager --channels-last"
+    #bash launch_benchmark.sh ${model} ${mode} "--num-iter 10 -m jit --channels-last"
 done
